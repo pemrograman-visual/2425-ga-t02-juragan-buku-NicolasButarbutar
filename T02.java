4d0fc8c40765828b37d961d6c91f9b2a414d0d47
Nicolas Butarbutar <nicolaszuliaaan@gmail.com>
2024-09-27 10:45:30 +0700

    Kami sudah menyelesaikan tugas.

--- T02.java
+++ T02.java
@@ -1,1 +1,48 @@
-// NIM - Your Name
+// 12S24038 - Nicolas J Grace Butarbutar
+// 12S24026 - Griselda Tabitha Nathania Hutahean
+
+import java.util.*;
+import java.lang.Math;
+
+public class T02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN, judul, penulis, penerbit, format, kategori;
+        int tahun, stok;
+        double harga, margin, rating;
+
+        iSBN = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        tahun = Integer.parseInt(input.nextLine());
+        penerbit = input.nextLine();
+        format = input.nextLine();
+        harga = Double.parseDouble(input.nextLine());
+        margin = Double.parseDouble(input.nextLine());
+        stok = Integer.parseInt(input.nextLine());
+        rating = Double.parseDouble(input.nextLine());
+        if (rating >= 4.7) {
+            kategori = "Best Pick";
+        } else {
+            if (rating >= 4.5 && rating < 4.7) {
+                kategori = "Must Read";
+            } else {
+                if (rating >= 4.0 && rating < 4.5) {
+                    kategori = "Recommended";
+                } else {
+                    if (rating >= 3.0 && rating < 4.0) {
+                        kategori = "Average";
+                    } else {
+                        kategori = "Low";
+                    }
+                }
+            }
+        }
+        System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + tahun + "|" + penerbit + "|" + format + "|" + harga + "|" + margin + "|" + stok + "|" + toFixed(rating,1) + "|" + kategori);
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
